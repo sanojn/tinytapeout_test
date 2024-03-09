d80a43fc32d89921610e716c110903f0eedde740
sanojn <58303304+sanojn@users.noreply.github.com>
2024-03-09 15:18:29 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,14 +11,14 @@ def hex(n): # Return binary integer equivalent of 2 BCD digits
 async def testCycle(dut,period):
     # Check one period
     for i in range(period,period,-1):
-      await ClockCycles(dut.clk, 1, false)
+      await ClockCycles(dut.clk, 1, False)
       assert dut.uo_out.value == hex(i)
     # Check one more period
     for i in range(period,period,-1):
-      await ClockCycles(dut.clk, 1, false)
+      await ClockCycles(dut.clk, 1, False)
       assert dut.uo_out.value == hex(i)
     # Multiple cycles
-    await ClockCycles(dut.clk, 3*period, false)
+    await ClockCycles(dut.clk, 3*period, False)
     assert dut.uo_out.value == hex(1)
 
 @cocotb.test()
@@ -34,7 +34,7 @@ async def test_adder(dut):
   dut.ui_in.value = 0
   dut.uio_in.value = 0
   dut.rst_n.value = 0
-  await ClockCycles(dut.clk, 10, false)
+  await ClockCycles(dut.clk, 10, False)
   dut.rst_n.value = 1
   assert dut.uo_out.value == hex(1)
 
