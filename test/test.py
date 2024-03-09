ff9cfe67bea266e67436c27d7ccf89f69a3cd590
sanojn <58303304+sanojn@users.noreply.github.com>
2024-03-09 20:50:30 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2023 Uri Shaked <uri@tinytapeout.com>
+_pro# SPDX-FileCopyrightText: © 2023 Uri Shaked <uri@tinytapeout.com>
 # SPDX-License-Identifier: MIT
 
 import cocotb
