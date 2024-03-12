d542708369e48161f1e22fc995b88c33db1df4e3
sanojn <58303304+sanojn@users.noreply.github.com>
2024-03-12 16:21:42 +0100

    Update seg7_digitsonly.v
    Changed segment order to conform with TT04+ Eval board

--- src/seg7_digitsonly.v
+++ src/seg7_digitsonly.v
@@ -1,20 +1,20 @@
 module seg7_digitsonly(
   input  [3:0] digit,
-  output [6:0] display
+  output [6:0] display // Segments g through a, in that order
 );
 
   always @(*) begin
     case (digit)
-      4'd0 :   display = 7'b1111110;
-      4'd1 :   display = 7'b0110000;
-      4'd2 :   display = 7'b1101101;
-      4'd3 :   display = 7'b1111001;
-      4'd4 :   display = 7'b0110011;
-      4'd5 :   display = 7'b1011011;
-      4'd6 :   display = 7'b1011111;
-      4'd7 :   display = 7'b1110000;
+      4'd0 :   display = 7'b0111111;
+      4'd1 :   display = 7'b0000110;
+      4'd2 :   display = 7'b1011011;
+      4'd3 :   display = 7'b1001111;
+      4'd4 :   display = 7'b1100110;
+      4'd5 :   display = 7'b1101101;
+      4'd6 :   display = 7'b1111101;
+      4'd7 :   display = 7'b0000111;
       4'd8 :   display = 7'b1111111;
-      4'd9 :   display = 7'b1111011;
+      4'd9 :   display = 7'b1101111;
       default: display = 7'bxxxxxxx;
     endcase  
   end
