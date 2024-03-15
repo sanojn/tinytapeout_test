9c9aaec14d26e644799be570deb53e2c7ae310ef
sanojn <58303304+sanojn@users.noreply.github.com>
2024-03-16 00:45:25 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -27,9 +27,14 @@ def releaseButtons(dut):
 async def testCycle(dut,period):
     await ClockCycles(dut.clk, 1, False) # allow for synch delay
     # allow input to be deglitched
-    if (dut.user_project.tick.value==0):
-      await RisingEdge(dut.user_project.tick)
-    await RisingEdge(dut.user_project.tick)
+#    if (dut.user_project.tick.value==0):
+#      await RisingEdge(dut.user_project.tick)
+#    await RisingEdge(dut.user_project.tick)
+    while (dut.user_project.tick.value==0):
+      await ClockCycles(dut.clk,1,False)
+    await ClockCycles(dut.clk,1,False)
+    while (dut.user_project.tick.value==0):
+      await ClockCycles(dut.clk,1,False)
     await ClockCycles(dut.clk, 2, False) # Let the debounce FSM see the tick
     # Now the counter should be rolling
     # Wait until it's 1 to simplify tests
