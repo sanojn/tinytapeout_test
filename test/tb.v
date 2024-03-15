e2424a33c9f1e1682fc543074cd5ffd6e794ac5e
sanojn <58303304+sanojn@users.noreply.github.com>
2024-03-15 23:17:52 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -96,8 +96,8 @@ module tb ();
   // them consistently
    wire [3:0] digit1, digit10;
   `ifdef GL_TEST
-     assign digit1  = { user_project.\digit1[3] , user_project.\digit1[2] , user_project.\digit1[1] , user_project.\digit1[0] }
-     assign digit10 = { user_project.\digit10[3] , user_project.\digit10[2] , user_project.\digit10[1] , user_project.\digit10[0] }
+     assign digit1  = { user_project.\digit1[3] , user_project.\digit1[2] , user_project.\digit1[1] , user_project.\digit1[0] };
+     assign digit10 = { user_project.\digit10[3] , user_project.\digit10[2] , user_project.\digit10[1] , user_project.\digit10[0] };
   `else
      assign digit1  = user_project.digit1;
      assign digit10 = user_project.digit10;
