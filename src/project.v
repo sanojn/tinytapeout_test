c53d32642641ddcaaf5b59c7a80913a62ca92b2b
sanojn <58303304+sanojn@users.noreply.github.com>
2024-03-22 03:02:36 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -136,7 +136,7 @@ module tt_um_sanojn_ttrpg_dice (
       .wen(wen),
       .wdata(wdata),
       .rdata_used(rdata_used),
-      .rdata(rdata)
+      .rdata(wdata)
     );
 
     // All output pins must be assigned. If not used, assign to 0.
